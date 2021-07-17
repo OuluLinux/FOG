7b6a92f6c72e12e70432693f475041512ff19bb4
Seppo Pakonen <seppo.pakonen@gmail.com>
2021-07-17 20:48:08 +0300

    FreeBSD makefile fix

--- Prim/PrimStrings0.cpp
+++ Prim/PrimStrings0.cpp
@@ -8,7 +8,7 @@
 //  		Compare two strings as for strcmp() but ignoring case inequalities.
 //  
 int strcasecmp(const char *p, const char *q) 
-#if defined flagCLANG && !defined flagFREEBSD
+#if defined flagCLANG && !defined flagFREEBSD && !defined flagLINUX
 throw()
 #endif
 {
